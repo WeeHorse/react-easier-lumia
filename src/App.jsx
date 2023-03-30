fa288eeeaeb8cd4c9a3f70d252a10b288d7345c3
no92one <linus.lindroth.92@gmail.com>
2023-03-30 15:50:44 +0200

    moved cart into shop

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ export default function App() {
                 <Navbar/>
                 <Outlet/>
             </div>
-            <Cart/>
         </div>
     )
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,11 @@
 import Search from "../components/Search.jsx";
 import Products from "../components/Products.jsx";
+import Cart from "../components/Cart.jsx";
 
 export default function () {
     return <>
         <Search/>
         <Products/>
+        <Cart/>
     </>
 }
