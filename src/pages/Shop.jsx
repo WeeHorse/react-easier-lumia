65a616733f57da6e8d24720f52d4eef79362694f
Ironboy <ironboy@users.noreply.github.com>
2023-04-06 12:49:34 +0200

    Cool

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,9 +3,11 @@ import Products from "../components/Products";
 
 export default function Shop() {
 
-  const s = useStates('store');
+  const s = useStates('store', { search: 'tak' });
+  const l = useStates({ counter: 0 });
 
   return <>
+    <button onClick={() => l.counter++}>{l.counter}</button>
     <input placeholder="Sök / Filtrera"  {...s.bind('search')} />
     < Products />
   </>;
