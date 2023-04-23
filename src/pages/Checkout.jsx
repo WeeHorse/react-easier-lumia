00fe200dda5911c417dcdfcd879287332de9ecd6
WeeHorse <benjaminberglund@gmail.com>
2023-04-24 00:16:32 +0200

    something

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,43 @@
-import {useStates} from 'react-easier'
+import {useStates, useFetch} from 'react-easier'
 import { objectifyForm } from "../modules/objectifyForm.js"
 
 export default function () {
 
-    const checkoutForm = useStates(useFetch('/user-data.json'));
+    const checkoutForm = useStates({
+        "email": "example@example.com",
+        "password": "supersecretpassword",
+        "street_address": "123 Main St",
+        "city": "Anytown",
+        "zip": "12345",
+        "states": [
+            {
+                "value": "AL",
+                "text": "Alabama"
+            },
+            {
+                "value": "AS",
+                "text": "Alaska"
+            },
+            {
+                "value": "AR",
+                "text": "Arkansas"
+            },
+            {
+                "value": "CA",
+                "text": "California"
+            },
+            {
+                "value": "TX",
+                "text": "Texas"
+            },
+            {
+                "value": "NY",
+                "text": "New York"
+            }
+        ]
+    }
+        // useFetch('/user-data.json')
+    );
 
     const watchFormState = () => {
         console.log('called')
@@ -19,28 +53,29 @@ export default function () {
 
     return <form id="checkout" onSubmit={submitCheckout} onChange={watchFormState}>
 
-        <label for="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input type="email" name="email" {...checkoutForm.bind('email')} />
 
-        <label for="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input type="password" name="password" {...checkoutForm.bind('password')} />
 
-        <label for="street-address">Address</label>
-        <input type="text" name="street-address" placeholder="1234 Main St" {...checkoutForm.bind('street-address')} />       
+        <label htmlFor="street-address">Address</label>
+        <input type="text" name="street-address" placeholder="1234 Main St" {...checkoutForm.bind('street_address')} />       
 
-        <label for="states">States</label>
-        <select name="states" multiple  {...checkoutForm.bind('states')}>
-        <option selected>Choose...</option>
-            <option value="al">Alabama</option>
-            <option value="as">Alaska</option>
-            <option value="ar">Arkansas</option>            
-            <option value="ca">California</option>
+        <label htmlFor="states">States</label>
+        <select name="states" multiple>
+            {checkoutForm.states.map(state=> <option value={state.value}>{state.text}</option>)}
+        {/* <option selected>Choose...</option>
+        <option value="AL"  {...checkoutForm.bind('states','al')}>Alabama</option>
+        <option value="AS"  {...checkoutForm.bind('states','as')}>Alaska</option>
+        <option value="AR"  {...checkoutForm.bind('states','ar')}>Arkansas</option>            
+        <option value="CA"  {...checkoutForm.bind('states','ca')}>California</option> */}
         </select>
 
-        <label for="city">City</label>
+        <label htmlFor="city">City</label>
         <input type="text" name="city" {...checkoutForm.bind('city')} />
 
-        <label for="zip">Zip</label>
+        <label htmlFor="zip">Zip</label>
         <input type="text" name="zip" {...checkoutForm.bind('zip')} />
         
         <button type="submit">Send</button>
